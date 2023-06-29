5a039448f8055acc309d3ee8f79282181a72ac0e
hsw <hsw840901@gamil.com>
2023-06-29 17:29:02 +0900

    simsimh2

--- 01.ProJect/00.Study/01.html/web/js/common.js
+++ 01.ProJect/00.Study/01.html/web/js/common.js
@@ -6,14 +6,144 @@
 let env = new Object();
 
 
-function fnCmnPrc(requstParam, callbackFn){
+function fnCmnPrc(objForm, reqParams, callbackFn){
 
-    for(key in requstParam) {
-        console.log(key);
-        console.log(requstParam[key]);
+    let resData = new Object();
+
+    // console.log("fnCmnPrc 데이터 처리 시작 >>>>");
+    // console.log("fnCmnPrc objForm",objForm);
+    // console.log("fnCmnPrc reqParams",reqParams);
+
+    for(key in reqParams) {
+        
+        console.log("fnCmnPrc for value",reqParams[key]);
+
+        this.fnSetData(objForm, key, reqParams[key]);
     } 
 
+    console.log("fnCmnPrc testId",Number(reqParams["testId"]));
+    console.log("fnCmnPrc testNm",Number(reqParams["testNm"]));
 
-    callbackFn();
+    resData.rmk  = Number(reqParams["testId"]) * Number(reqParams["testNm"]);
+    resData.rmk2 = Number(reqParams["testId"]) + Number(reqParams["testNm"]);
 
+    console.log("fnCmnPrc resData",resData);
+
+    callbackFn(resData);
+}
+
+/*
+fnSetData 
+요청 Params : form, 오브젝트 id, 값
+*/
+function fnSetData(objForm, key, value){
+    //document.forms[frmName][key].setAttribute('value',value);
+
+    // console.log("fnSetData 오브젝트 입력 값 넣기 시작 >>>>");
+    // console.log("fnSetData objForm ",objForm);
+    // console.log("fnSetData key",key);
+    // console.log("fnSetData value",value);
+
+    // 존재하는 경우에만
+    if ( this.fnGetObj(objForm, key) ){
+        document.forms[objForm.name][key].setAttribute('value',value);
+    }
+}
+
+/*
+fnGetData 
+요청 Params : form, 오브젝트 id
+*/
+function fnGetData(objForm, key){
+
+    let rtnVal = "";
+
+    // console.log("fnGetData 오브젝트 입력 값 가져오기 시작 >>>>");
+    // console.log("fnGetData objForm ", objForm);
+    // console.log("fnGetData key ", key);
+
+    // 존재하는 경우에만
+    if ( this.fnGetObj(objForm, key) ){
+        rtnVal = document.forms[objForm.name][key].value;
+    }    
+
+    return rtnVal;
+}
+
+/*
+fnGetObj 
+요청 Params : form, 오브젝트 id
+*/
+function fnGetObj(objForm, key){
+
+    let rtnVal = "";
+
+    // console.log("fnGetObj 오브젝트 존재 유무 확인 시작 >>>>");
+    // console.log("fnGetObj objForm ",objForm);
+    // console.log("fnGetObj key",key);
+
+    var objType = typeof document.forms[objForm.name][key];
+
+    if ( objType == "undefined"){
+        rtnVal = false;
+    }else{
+        console.log(document.forms[objForm.name][key]);
+        rtnVal = document.forms[objForm.name][key].getAttribute("type");
+    }
+    return rtnVal;
+
+}
+
+
+function fnAddEventListener(objForm, key, eventType, eventFnc){
+
+    // console.log("fnAddEventListener 이벤트 추가 시작 >>>>");
+    // console.log("fnAddEventListener objForm",objForm);
+    // console.log("fnAddEventListener key",key);
+    // console.log("fnAddEventListener eventType",eventType);
+    // console.log("fnAddEventListener eventFnc",eventFnc);
+
+
+    if ( this.fnGetObj(objForm, key) ){
+        document.forms[objForm.name][key].addEventListener(eventType, function fnc(){
+            (new Function(  eventFnc + '();' ))();
+        })
+    }  
+}
+
+function fnkeydownEvt(event){
+    console.log(event);
+}
+
+
+/*
+fnSetObjtoJson 
+요청 Params : form
+*/
+function fnSetObjtoJson(objForm){
+
+    let jsonData = null;
+
+    let objLen = document.forms[objForm.name].length;
+
+    let obj = new Object();
+
+    if ( objLen > 0 ){
+
+        for( var i=0; i<objLen; i++){
+            
+            let objName = document.forms[objForm.name][i];
+
+            let objType = this.fnGetObj(objForm, objName.id)
+
+            if ( objType == "text" ){
+                
+                obj[objName.id] = objName.value;
+            }            
+        }
+
+        jsonData = obj;
+    }
+
+    return jsonData;
 }
